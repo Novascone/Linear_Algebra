61a8cb7db4653a140755e3346017995a800e2bed
Warren <Warren.Novascone@proton.me>
2023-11-13 23:09:22 +0000

    Update transpose.py

--- Matrices/transpose.py
+++ Matrices/transpose.py
@@ -3,7 +3,8 @@ import numpy as np
 A = np.array([[2, 4],
                [0, 3]])
 
-B = np.array([[-4, -1],
-               [1, 3]])
+B = np.array([[2, 1],
+               [2, 3]])
+v = np.array([2,3])
 
-print(np.trace(-3*A + 5*B))
+print(np.dot(v.T,B))

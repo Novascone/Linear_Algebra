287909dff13f71ed1dd90b06f48c77c3b3e7468e
Warren <Warren.Novascone@proton.me>
2023-11-15 07:06:31 +0000

    Update transpose.py

--- Matrices/transpose.py
+++ Matrices/transpose.py
@@ -1,10 +1,14 @@
 import numpy as np
 
-A = np.array([[2, 4],
-               [0, 3]])
+A = np.array([[2, 4, 3],
+               [0, 1, 3]])
 
-B = np.array([[2, 1],
-               [2, 3]])
+B = np.array([[2, 0],
+               [4, 1],
+               [3, 3]])
 v = np.array([2,3])
 
-print(np.dot(v.T,B))
+C = np.array([[-2, -1, 3],
+               [6, -7, 7]])
+
+print(C.T)

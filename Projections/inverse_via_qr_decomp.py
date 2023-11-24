4c9fcd0504d00801180ba9ebff662ce824182679
Warren <Warren.Novascone@proton.me>
2023-11-24 13:58:17 +0000

    Update inverse_via_qr_decomp.py

--- Projections/inverse_via_qr_decomp.py
+++ Projections/inverse_via_qr_decomp.py
@@ -5,6 +5,8 @@ n = 100
 
 A = np.random.randint(0,5,size=(m,n))
 
-A_inv = np.linalg.inv(A)
+Q,R = np.linalg.qr(A,'complete')
+A_inv = np.linalg.inv(R)*Q.T
+A_inv_np = np.linalg.inv(A)
 
-print(A_inv)
+print('A Inverse QR - A Inverse: ',A_inv - A_inv_np), print(' ')

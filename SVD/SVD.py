7f34bbb8f590e1dbed99a4da8ee848a851fced5b
Warren <Warren.Novascone@proton.me>
2023-12-02 18:17:04 +0000

    Update SVD.py

--- SVD/SVD.py
+++ SVD/SVD.py
@@ -1,1 +1,32 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+A = [[3,0,5], [8,1,3]]
+
+U,S,V = np.linalg.svd(A)
+
+print(U), print(' ')
+print(S), print(' ')
+print(V)
+
+plt.subplot(141)
+plt.imshow(A)
+plt.title('A')
+plt.axis('off')
+
+plt.subplot(142)
+plt.imshow(U)
+plt.title('U')
+plt.axis('off')
+
+plt.subplot(143)
+plt.imshow(np.diag(S))
+plt.title('$\Sigma$')
+plt.axis('off')
+
+plt.subplot(144)
+plt.imshow(V)
+plt.title('V')
+plt.axis('off')
+
+plt.show() 

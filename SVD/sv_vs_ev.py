a81e27d6cad6674519bc0d2908cd3c4e56fcc7fd
Warren <Warren.Novascone@proton.me>
2023-12-03 14:45:18 +0000

    Update sv_vs_ev.py

--- SVD/sv_vs_ev.py
+++ SVD/sv_vs_ev.py
@@ -1,10 +1,29 @@
 import numpy as np
 
-# case 1: eig(A'A) vs svd(A)
+# case 1: eig(A'A) vs. svd(A)
 
-A = np.array([[3,1,0],[1,1,0]])
+# A = np.array([[3,1,0],[1,1,0]])
 
-print(np.sort(np.linalg.eig(A.T@A)[0]))
-print(np.sort(np.linalg.svd(A)[1])**2)
+# print(np.sort(np.linalg.eig(A.T@A)[0]))
+# print(np.sort(np.linalg.svd(A)[1])**2)
 
-# case 2:
+# case 2: eig(A'A) vs. svd(A'A)
+
+
+
+# print(np.sort(np.linalg.eig(A.T@A)[0]))
+# print(np.sort(np.linalg.svd(A.T@A)[1]))
+
+# case 3a: eig(A) vs. svd(A), real-valued eigs
+
+# A = np.array([[3,1,0],[1,1,0], [1,1,1]])
+
+# print(np.sort(np.linalg.eig(A)[0]))
+# print(np.sort(np.linalg.svd(A)[1]))
+
+# case 3b: eig(A) vs svd(A), complex eigs
+
+A = np.random.randn(3,3)
+
+print(np.sort(np.linalg.eig(A)[0]))
+print(np.sort(np.linalg.svd(A)[1]))

8de38f2a03a1af59b525b10fe3e530795f795208
Warren <Warren.Novascone@proton.me>
2023-12-09 14:45:02 +0000

    Update pca.py

--- Quadratic/pca.py
+++ Quadratic/pca.py
@@ -19,7 +19,7 @@ for i in range(M):
     
     # add noise
     
-    data = data + np.random.randn(M,N)/3
+    data = data + np.random.randn(M,N)/10
     
     for i in range(M):
         data[i,:] = data[i,:] - np.mean(data[i,:])
@@ -39,4 +39,47 @@ ax[1].imshow(cm)
 ax[1].set_ylabel('Channels')
 ax[1].set_xlabel('Channels')
 
+plt.show()
+
+evals, evecs = np.linalg.eig(cm)
+
+idx = np.argsort(evals)[::-1]
+evals = np.real(evals[idx])
+evecs = evecs[:,idx]
+
+evals = 100*evals/np.sum(evals)
+
+r = 2
+
+comp_time_series = evecs[:,:r].T@data
+
+fig = plt.subplots(121,figsize=(10,4))
+
+plt.subplot(121)
+plt.plot(evals,'s-')
+plt.xlabel('Component number')
+plt.ylabel('$\lambda$ (% total variance)')
+plt.title('Eigenspectrum')
+
+plt.subplot(122)
+plt.plot(evecs[:,0],label='PC1')
+plt.plot(evecs[:,1],label='PC2')
+plt.xlabel('Channel')
+plt.ylabel('PC weight')
+plt.title('Eigenvectors')
+plt.legend()
+plt.show()
+
+plt.plot(chanrel)
+plt.xlabel('Channel')
+plt.ylabel('Channel weight')
+plt.title('Ground truth channel weights')
+plt.show()
+
+plt.plot(comp_time_series[0,:],label='PC1')
+plt.plot(comp_time_series[1,:],label='PC2')
+plt.xlabel('Time (a.u)')
+plt.ylabel('Activity')
+plt.title('Time course plots')
+plt.legend()
 plt.show()

0ee1495ea2de924b5cc0275cc48b0ac6e700318d
Warren <Warren.Novascone@proton.me>
2023-12-10 11:25:49 +0000

    Update qfgene.py

--- Quadratic/qfgene.py
+++ Quadratic/qfgene.py
@@ -22,28 +22,3 @@ B = B@B.T
 evals,evecs = linalg.eigh(A,B)
 
 
-plt.subplots(231)
-plt.imshow(evecs.T@evecs,extent=[-m,m,-m,m])
-plt.xticks([])
-plt.title('W$^T$W')
-
-tmp = evecs.T@evecs
-plt.subplots(234)
-plt.plot(tmp[1,:],'s-')
-plt.title('W$_j^T$W')
-
-plt.subplots(232)
-plt.imshow(evecs.T@A@evecs,extent=[-m,m,-m,m])
-plt.xticks([])
-plt.title('W$^T$AW')
-
-plt.subplots(235)
-plt.plot(np.diag(evecs.T@A@evecs),'s-')
-plt.plot(evals,'r')
-plt.title('diag(W$^T$AW)')
-
-plt.subplots(236)
-plt.plot(np.diag(evecs.T@B@evecs),'s-')
-plt.title('diag(W$^T$BW)')
-
-plt.show()
